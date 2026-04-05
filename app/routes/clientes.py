b91d2b311a7a74fe3822944e8a8540988be6827e
mario-tapia-dev <mario.tapia.dev@gmail.com>
2026-04-05 02:19:33 -0600

    feat: Modulo completo de clientes con pruebas ya realizadas y documentadas en postman

--- app/routes/clientes.py
+++ app/routes/clientes.py
@@ -47,19 +47,37 @@ def obtener_cliente(id_cliente):
         cur = conn.cursor()
         cur.execute("""
             SELECT
-                c.id_cliente,
-                c.folio,
-                c.nombre,
-                c.apellido_paterno,
-                c.apellido_materno,
-                c.telefono,
-                c.email,
+                clientes.id_cliente,
+                clientes.folio,
+                clientes.nombre,
+                clientes.apellido_paterno,
+                clientes.apellido_materno,
+                clientes.telefono,
+                clientes.email,
                 e.nombre AS estado,
-                m.nombre AS municipio
-            FROM clientes c
-            LEFT JOIN estado e ON c.id_estado = e.id_estado
-            LEFT JOIN municipio m ON c.id_municipio = m.id_municipio
-            WHERE c.id_cliente = %s
+                m.nombre AS municipio,
+                COALESCE(
+                    JSON_AGG(
+                        JSON_BUILD_OBJECT('id_cat', c.id_cat, 'nombre', c.nombre)
+                    ) FILTER (WHERE c.id_cat IS NOT NULL),
+                    '[]'
+                ) AS categorias
+            FROM clientes 
+            LEFT JOIN estados e ON clientes.id_estado = e.id_estado
+            LEFT JOIN municipios m ON clientes.id_municipio = m.id_municipio
+            LEFT JOIN clientes_categoria cltes_cat ON cltes_cat.id_cliente = clientes.id_cliente
+            LEFT JOIN categorias c ON c.id_cat = cltes_cat.id_cat 
+            WHERE clientes.id_cliente = %s
+            GROUP BY 
+                clientes.id_cliente,
+                clientes.folio,
+                clientes.nombre,
+                clientes.apellido_paterno,
+                clientes.apellido_materno,
+                clientes.telefono,
+                clientes.email,
+                e.nombre,
+                m.nombre
         """, (id_cliente,))
         cliente = cur.fetchone()
         if cliente is None:
@@ -87,9 +105,69 @@ def crear_cliente():
         for campo in campos_requeridos:
             if not data.get(campo):
                 return error(message=f"El campo '{campo}' es obligatorio", status=400)
+        
+        categorias_ids = data.get("categorias_ids", [])
 
         conn = get_connection()
         cur = conn.cursor()
+
+        # Verificar que el estado exista
+        if data.get("id_estado") is not None:
+            cur.execute("""
+            SELECT 
+                id_estado 
+            FROM estados
+            WHERE id_estado = %s
+            """, (data.get("id_estado"),))
+            estado = cur.fetchone()
+            if estado is None:
+                return error(message="El estado seleccionado no existe", status=404)
+
+        # Verificar que el municipio exista
+        if data.get("id_municipio") is not None:
+            cur.execute("""
+            SELECT
+                id_municipio
+            FROM municipios
+            WHERE id_municipio = %s
+            """, (data.get("id_municipio"),))
+            municipio = cur.fetchone()
+            if municipio is None:
+                return error(message="El municipio seleccionado no existe", status=404)
+        
+        # Verificar que el folio no esté repetido
+        cur.execute("""
+        SELECT
+            folio
+        FROM clientes
+        WHERE folio = %s 
+        """, (data["folio"],))
+        
+        if cur.fetchone() is not None:
+            return error(message="El folio asignado ya existe, coloque otro diferente", status=409)
+
+        # Verificar que el telefono no esté repetido
+        cur.execute("""
+        SELECT
+            telefono
+        FROM clientes
+        WHERE telefono = %s 
+        """, (data["telefono"],))
+        
+        if cur.fetchone() is not None:
+            return error(message="El telefono asignado ya existe, coloque otro diferente", status=409)
+        
+        # Verificar que el email no esté repetido
+        cur.execute("""
+        SELECT
+            email
+        FROM clientes
+        WHERE email = %s 
+        """, (data["email"],))
+        
+        if cur.fetchone() is not None:
+            return error(message="El email asignado ya existe, coloque otro diferente", status=409)
+
         cur.execute("""
             INSERT INTO clientes (
                 folio, nombre, apellido_paterno, apellido_materno,
@@ -104,10 +182,19 @@ def crear_cliente():
             data["telefono"],
             data["email"],
             data.get("id_estado"),         # Opcional
-            data.get("id_municipio")       # Opcional
+            data.get("id_municipio"),       # Opcional
         ))
-        conn.commit()
+
         nuevo_id = cur.fetchone()["id_cliente"]
+
+        for id_cat in categorias_ids:
+            cur.execute("""
+            INSERT INTO clientes_categoria (
+                id_cat, id_cliente
+            ) VALUES (%s, %s)
+            """, (id_cat, nuevo_id))
+
+        conn.commit()
         return success(data={"id_cliente": nuevo_id}, message="Cliente creado correctamente", status=201)
     except Exception as e:
         if conn:
@@ -136,6 +223,66 @@ def actualizar_cliente(id_cliente):
         if cur.fetchone() is None:
             return error(message="Cliente no encontrado", status=404)
 
+        # Verificar que el estado exista
+        if data.get("id_estado") is not None:
+            cur.execute("""
+            SELECT 
+                id_estado 
+            FROM estados
+            WHERE id_estado = %s
+            """, (data.get("id_estado"),))
+            estado = cur.fetchone()
+            if estado is None:
+                return error(message="El estado seleccionado no existe", status=404)
+
+        # Verificar que el municipio exista
+        if data.get("id_municipio") is not None:
+            cur.execute("""
+            SELECT
+                id_municipio
+            FROM municipios
+            WHERE id_municipio = %s
+            """, (data.get("id_municipio"),))
+            municipio = cur.fetchone()
+            if municipio is None:
+                return error(message="El municipio seleccionado no existe", status=404)
+        
+        # Verificar que el folio no esté repetido
+        cur.execute("""
+        SELECT
+            folio
+        FROM clientes
+        WHERE folio = %s 
+        """, (data.get("folio"),))
+        
+        if cur.fetchone() is not None:
+            return error(message="El folio asignado ya existe, coloque otro diferente", status=409)
+
+        # Verificar que el telefono no esté repetido
+        cur.execute("""
+        SELECT
+            telefono
+        FROM clientes
+        WHERE telefono = %s 
+        """, (data.get("telefono"),))
+        
+        if cur.fetchone() is not None:
+            return error(message="El telefono asignado ya existe, coloque otro diferente", status=409)
+        
+        # Verificar que el email no esté repetido
+        cur.execute("""
+        SELECT
+            email
+        FROM clientes
+        WHERE email = %s 
+        """, (data.get("email"),))
+        
+        if cur.fetchone() is not None:
+            return error(message="El email asignado ya existe, coloque otro diferente", status=409)
+
+        cur.execute("SELECT * FROM clientes WHERE id_cliente = %s" , (id_cliente,))
+        cliente_actual = cur.fetchone()
+
         cur.execute("""
             UPDATE clientes SET
                 folio = %s,
@@ -148,16 +295,25 @@ def actualizar_cliente(id_cliente):
                 id_municipio = %s
             WHERE id_cliente = %s
         """, (
-            data["folio"],
-            data["nombre"],
-            data["apellido_paterno"],
-            data.get("apellido_materno"),
-            data["telefono"],
-            data["email"],
-            data.get("id_estado"),
-            data.get("id_municipio"),
+            data.get("folio", cliente_actual["folio"]),
+            data.get("nombre", cliente_actual["nombre"]),
+            data.get("apellido_paterno", cliente_actual["apellido_paterno"]),
+            data.get("apellido_materno", cliente_actual["apellido_materno"]),
+            data.get("telefono", cliente_actual["telefono"]),
+            data.get("email", cliente_actual["email"]),
+            data.get("id_estado", cliente_actual["id_estado"]),
+            data.get("id_municipio", cliente_actual["id_municipio"]),
             id_cliente
         ))
+
+        if "categorias_ids" in data:
+            cur.execute("DELETE FROM clientes_categoria WHERE id_cliente = %s", (id_cliente,))
+            for id_cat in data["categorias_ids"]:
+                cur.execute("""
+                    INSERT INTO clientes_categoria (id_cat, id_cliente)
+                    VALUES (%s, %s)
+                """, (id_cat, id_cliente))
+
         conn.commit()
         return success(message="Cliente actualizado correctamente")
     except Exception as e:
@@ -185,6 +341,7 @@ def eliminar_cliente(id_cliente):
         if cur.fetchone() is None:
             return error(message="Cliente no encontrado", status=404)
 
+        cur.execute("DELETE FROM clientes_categoria WHERE id_cliente = %s", (id_cliente,))
         cur.execute("DELETE FROM clientes WHERE id_cliente = %s", (id_cliente,))
         conn.commit()
         return success(message="Cliente eliminado correctamente")
