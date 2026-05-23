3bdac14df030e7c0926be71dfcbb6c8fb27c4f52
mario-tapia-dev <mario.tapia.dev@gmail.com>
2026-05-22 23:46:58 -0600

    feat: Cambiamos de SMTP a la api de resend para manejar los correos

--- app/utils/email.py
+++ app/utils/email.py
@@ -1,35 +1,43 @@
-import smtplib
 import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
- 
+import requests
+
 def enviar_alerta_stock(descripcion_producto, nombre_almacen, stock_actual, min_stock):
+
+    api_key = os.getenv("RESEND_API_KEY")
     remitente = os.getenv("MAIL_SENDER")
     destinatario = os.getenv("MAIL_RECEIVER")
-    password = os.getenv("MAIL_PASSWORD")
- 
+
     asunto = f"⚠️ Alerta de stock mínimo — {descripcion_producto}"
- 
+
     cuerpo = f"""
     Se ha detectado que el siguiente producto ha alcanzado su stock mínimo:
- 
+
     Producto:       {descripcion_producto}
     Almacén:        {nombre_almacen}
     Stock actual:   {stock_actual}
     Stock mínimo:   {min_stock}
- 
+
     Por favor tome las medidas necesarias para reabastecer el inventario.
     """
- 
-    msg = MIMEMultipart()
-    msg["Subject"] = asunto
-    msg["From"] = remitente
-    msg["To"] = destinatario
-    msg.attach(MIMEText(cuerpo, "plain"))
- 
+
     try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(remitente, password)
-            smtp.sendmail(remitente, destinatario, msg.as_string())
+
+        response = requests.post(
+            "https://api.resend.com/emails",
+            headers={
+                "Authorization": f"Bearer {api_key}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "from": remitente,
+                "to": destinatario,
+                "subject": asunto,
+                "text": cuerpo
+            }
+        )
+
+        print(response.status_code)
+        print(response.text)
+
     except Exception as e:
         print(f"Error al enviar alerta de stock: {str(e)}")
