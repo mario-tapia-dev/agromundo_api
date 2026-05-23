f0a7d3d03193db9cd3b35ccb8c4efe8a1cbacb4e
Ricardo-511 <ricgut06@gmail.com>
2026-05-22 20:56:41 -0600

    Arregla error de comillas al añadir columna y se le asigna como campo no nulo

--- migrations/008_agregar_moneda.sql
+++ migrations/008_agregar_moneda.sql
@@ -1,5 +1,5 @@
 -- Agregar columna moneda para tipo de moneda
-ALTER TABLE productos ADD COLUMN moneda VARCHAR(3) DEFAULT "MXN";
+ALTER TABLE productos ADD COLUMN moneda VARCHAR(3) NOT NULL DEFAULT 'MXN';
 
 -- Agrega una restricción para limitar a pesos y dólares
 ALTER TABLE productos ADD CONSTRAINT check_moneda CHECK (moneda IN ('MXN', 'USD'));
